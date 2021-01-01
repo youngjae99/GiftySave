bcaa3b2f3952c669705bb9a636f1d901a274e669
youngjae <elite1111cyj@gmail.com>
2021-01-01 22:48:04 +0900

    Initial Commit

---
+++ giftysave/giftysaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  giftysaveApp.swift
+//  giftysave
+//
+//  Created by Youngjae Jang on 2021/01/01.
+//
+
+import SwiftUI
+
+@main
+struct giftysaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
