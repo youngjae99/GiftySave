6e62d2a301ecc95257c7d795c768f7a430e37d3b
youngjae <elite1111cyj@gmail.com>
2021-01-02 15:41:05 +0900

    button add

--- giftysave/ContentView.swift
+++ giftysave/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("기프티콘 관리를 쉽게")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .bold()
+                .padding()
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("gifticon 추가하기")
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone X")
     }
 }
