f35fa680433b197acef3a150b9ba9598afdf9ae6
youngjae <elite1111cyj@gmail.com>
2021-01-03 16:23:46 +0900

    list

--- giftysave/ContentView.swift
+++ giftysave/ContentView.swift
@@ -10,10 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("기프티콘 관리를 쉽게")
+            Text("GiftySave")
                 .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 .bold()
                 .padding()
+            List {
+                Text("기프티콘1")
+                Text("기프티콘2")
+                Text("기프티콘3")
+                Text("기프티콘4")
+            }
+            
             Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
                 Text("gifticon 추가하기")
             }
